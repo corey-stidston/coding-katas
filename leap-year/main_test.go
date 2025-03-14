0450005241b651862472d0a409ab1d9174a841b2
corey-stidston <corey.stidston@outlook.com>
2025-03-15 08:31:51 +1100

    feat: add test for typical non leap year

--- leap-year/main_test.go
+++ leap-year/main_test.go
@@ -2,9 +2,18 @@ package main
 
 import "testing"
 
-func TestReturnsTrueForLeapYear(t *testing.T) {
+func TestReturnsTrueForTypicalLeapYear(t *testing.T) {
 	expected := true
 	result := isLeapYear(1996)
+
+	if result != expected {
+		t.Errorf("Expected %t but got %t", expected, result)
+	}
+}
+
+func TestReturnsFalseForTypicalNonLeapYear(t *testing.T) {
+	expected := false
+	result := isLeapYear(2001)
 
 	if result != expected {
 		t.Errorf("Expected %t but got %t", expected, result)
