74943d387fa170a58390a9585dd0664318492525
corey-stidston <corey.stidston@outlook.com>
2025-03-19 21:19:20 +1100

    test: add failing test for getting item quantity

--- shopping-basket/main_test.go
+++ shopping-basket/main_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestAddItemsToShoppingBasket(t *testing.T) {
-	basket := ShoppingBasket{}
+	basket := ShoppingBasket {}
 	basket.AddItem("orange")
 
 	if(len(basket.Basket) != 1 || basket.Basket[0] != "orange") {
 		t.Errorf("Expected basket to contain exactly 1 item that is an orange")
 	}
-	
+}
+
+func TestGetQuantity(t *testing.T) {
+	basket := ShoppingBasket {
+		Basket: []string{"orange", "orange"},
+	}
+
+	expected := 2
+	numberOfOranges := basket.GetItemQuantity("orange")
+
+	if (numberOfOranges != expected) {
+		t.Errorf("Expected an item quantity of %d oranges", expected)
+	}
 }
