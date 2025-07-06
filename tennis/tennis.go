ec0015f14ef172d1034698eccf3bd878247edd60
corey-stidston <corey.stidston@outlook.com>
2025-07-06 18:29:10 +1000

    feat: love fifteen and refactor

--- tennis/tennis.go
+++ tennis/tennis.go
@@ -5,33 +5,41 @@ The tennis society has contracted you to build a scoreboard to display the curre
 You can read more about Tennis scores on wikipedia which is summarized below:
 
 1. A game is won by the first player to have won at least four points in total and at least two points more than the opponent.
-2. The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as 
+2. The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as
 “Love”, “Fifteen”, “Thirty”, and “Forty” respectively.
 3. If at least three points have been scored by each player, and the scores are equal, the score is “Deuce”.
-4. If at least three points have been scored by each side and a player has one more point than his opponent, 
+4. If at least three points have been scored by each side and a player has one more point than his opponent,
 the score of the game is “Advantage” for the player in the lead.
 You need only report the score for the current game. Sets and Matches are out of scope.
 */
 
 package main
 
-type Score int
+type Score string
 
 const (
-	WinLose Score = iota
-	LoseWin
-	Deuce
+	Win       Score = "Win"
+	Lose      Score = "Lose"
+	Deuce     Score = "Deuce"
+	Love      Score = "Love"
+	Fifteen   Score = "Fifteen"
+	Thirty    Score = "Thirty"
+	Forty     Score = "Forty"
+	Advantage Score = "Advantage"
 )
 
-func score(player1 int, player2 int) (Score) {
-	if (player1 >= 4 && player1 - player2 >= 2) {
-		return WinLose
-	} else if(player2 >= 4 && player2 - player1 >= 2) {
-		return LoseWin
-	} else if(player1 == player2 && player1 >= 3 && player2 >= 3) {
-		return Deuce
+var scoreMap = [4]Score{Love, Fifteen, Thirty, Forty}
+
+func score(player1 int, player2 int) (Score, Score) {
+	if player1 >= 4 && player1-player2 >= 2 {
+		return Win, Lose
+	} else if player2 >= 4 && player2-player1 >= 2 {
+		return Lose, Win
+	} else if player1 == player2 && player1 >= 3 && player2 >= 3 {
+		return Deuce, Deuce
 	}
-	return LoseWin
+
+	return scoreMap[player1], scoreMap[player2]
 }
 
 func main() {
--- tennis/tennis_test.go
+++ tennis/tennis_test.go
@@ -9,66 +9,89 @@ func TestScore(t *testing.T) {
 	tests := []struct {
 		player1Points int
 		player2Points int
-		expectedScore Score
+		player1ExpectedScore Score
+		player2ExpectedScore Score
 	}{
 		{
 			player1Points: 4,
 			player2Points: 0,
-			expectedScore: WinLose,
+			player1ExpectedScore: Win,
+			player2ExpectedScore: Lose,
 		},
 		{
 			player1Points: 4,
 			player2Points: 1,
-			expectedScore: WinLose,
+			player1ExpectedScore: Win,
+			player2ExpectedScore: Lose,
 		},
 		{
 			player1Points: 4,
 			player2Points: 2,
-			expectedScore: WinLose,
+			player1ExpectedScore: Win,
+			player2ExpectedScore: Lose,
 		},
 		{
 			player1Points: 6,
 			player2Points: 4,
-			expectedScore: WinLose,
+			player1ExpectedScore: Win,
+			player2ExpectedScore: Lose,
 		},
 		{
 			player1Points: 0,
 			player2Points: 4,
-			expectedScore: LoseWin,
+			player1ExpectedScore: Lose,
+			player2ExpectedScore: Win,
 		},
 		{
 			player1Points: 1,
 			player2Points: 4,
-			expectedScore: LoseWin,
+			player1ExpectedScore: Lose,
+			player2ExpectedScore: Win,
 		},
 		{
 			player1Points: 2,
 			player2Points: 4,
-			expectedScore: LoseWin,
+			player1ExpectedScore: Lose,
+			player2ExpectedScore: Win,
 		},
 		{
 			player1Points: 4,
 			player2Points: 6,
-			expectedScore: LoseWin,
+			player1ExpectedScore: Lose,
+			player2ExpectedScore: Win,
 		},
 		{
 			player1Points: 3,
 			player2Points: 3,
-			expectedScore: Deuce,
+			player1ExpectedScore: Deuce,
+			player2ExpectedScore: Deuce,
 		},
 		{
 			player1Points: 4,
 			player2Points: 4,
-			expectedScore: Deuce,
+			player1ExpectedScore: Deuce,
+			player2ExpectedScore: Deuce,
+		},
+		{
+			player1Points: 0,
+			player2Points: 1,
+			player1ExpectedScore: Love,
+			player2ExpectedScore: Fifteen,
+		},
+		{
+			player1Points: 1,
+			player2Points: 0,
+			player1ExpectedScore: Fifteen,
+			player2ExpectedScore: Love,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(fmt.Sprint(tt), func(t *testing.T) {
-			score := score(tt.player1Points, tt.player2Points)
+			score1, score2 := score(tt.player1Points, tt.player2Points)
 
-			if score != tt.expectedScore {
-				t.Errorf("Expected %d but got %d", tt.expectedScore, score)
+			if score1 != tt.player1ExpectedScore && score2 != tt.player2ExpectedScore {
+				t.Errorf("Expected %s - %s but got %s - %s", tt.player1ExpectedScore, score1, tt.player2ExpectedScore, score2)
 			}
 		})
 	}
