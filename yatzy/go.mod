bb27cc7574aba18446bf42bd2ebd5c64ddf171cc
corey-stidston <corey.stidston@outlook.com>
2025-07-07 16:55:45 +1000

    feat: yatzy skeleton

---
+++ yatzy/go.mod
@@ -0,0 +1,3 @@
+module github.com/corey-stidston/coding-katas/yatzy
+
+go 1.21
---
+++ yatzy/yatzy.go
@@ -0,0 +1,25 @@
+/*
+The game of Yatzy is a simple dice game. Each player rolls five six-sided dice. 
+They can re-roll some or all of the dice up to three times (including the original roll).
+
+The player then places the roll in a category, such as ones, twos, fives, pair, two pairs etc (see the rules below). 
+If the roll is compatible with the category, the player gets a score for the roll according to the rules. 
+If the roll is not compatible with the category, the player scores zero for the roll.
+
+For example, suppose a player scores 5,6,5,5,2 in the fives category they would score 15 (three fives). 
+The score for that go is then added to their total and the category cannot be used again in the remaining goes for that game. 
+A full game consists of one go for each category. Thus, for their last go in a game, a player must choose their only remaining category.
+
+Your task is to score a GIVEN roll in a GIVEN category. You do NOT have to program the random dice rolling. 
+The game is NOT played by letting the computer choose the highest scoring category for a given roll.
+*/
+
+package main
+
+func getHelloMessage() string {
+	return "Hello, World!"
+}
+
+func main() {
+	//
+}
